f7947ced50cd29882ae99ccea10becdba0d4ee73
Lebedev Vladimir <goldswanru@gmail.com>
2021-06-21 15:39:13 +0300

    Auto validation of anti-forgery token is enabled globally

--- designer-website/Startup.cs
+++ designer-website/Startup.cs
@@ -6,6 +6,7 @@ using designer_website.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,7 +25,8 @@ namespace designer_website
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews(options =>
+                options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute()));
             services.AddDbContext<MSDBcontext>();
         }
 
