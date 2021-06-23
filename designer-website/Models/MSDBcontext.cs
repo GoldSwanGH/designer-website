d609e3630dfd280d904458e7414fd3e3cd807643
Lebedev Vladimir <goldswanru@gmail.com>
2021-06-23 21:50:25 +0300

    Database connection string is hidden in appsettings.json

--- designer-website/Models/MSDBcontext.cs
+++ designer-website/Models/MSDBcontext.cs
@@ -29,8 +29,7 @@ namespace designer_website.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=DESKTOP-EHI5BK7\\SQLEXPRESS;Database=designer-website;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer("Name=ConnectionStrings:DesignerWebsiteDatabase");
             }
         }
 
