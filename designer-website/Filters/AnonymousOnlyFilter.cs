f58d478d9f71742e27f966d76820817e21613335
Lebedev Vladimir <goldswanru@gmail.com>
2021-06-24 15:28:26 +0300

    AnonymousOnlyFilter added

---
+++ designer-website/Filters/AnonymousOnlyFilter.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace designer_website.Filters
+{
+    public class AnonymousOnlyFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (context.HttpContext.User.Identity.IsAuthenticated)
+            {
+                context.Result = new RedirectToActionResult("Index", "Home","");
+            }
+        }
+    }
+}
