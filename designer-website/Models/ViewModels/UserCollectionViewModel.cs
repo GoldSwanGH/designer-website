a05f2c1857b3a72fa60ec69895c01b1869b2427e
Lebedev Vladimir <goldswanru@gmail.com>
2021-06-26 18:23:28 +0300

    UserCollectionViewModel added

---
+++ designer-website/Models/ViewModels/UserCollectionViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace designer_website.Models
+{
+    public class UserCollectionViewModel
+    {
+        public IEnumerable<UserViewModel> Users { get; set; }
+    }
+}
