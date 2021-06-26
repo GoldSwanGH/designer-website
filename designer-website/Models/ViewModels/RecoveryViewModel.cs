de0d10282e67c5649a09438d0adef9dec2f9c11a
Lebedev Vladimir <goldswanru@gmail.com>
2021-06-26 17:53:29 +0300

    Password recovery implementation

---
+++ designer-website/Models/ViewModels/RecoveryViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using designer_website.Attributes;
+using designer_website.Models.EntityFrameworkModels;
+
+namespace designer_website.Models
+{
+    public class RecoveryViewModel : UserViewModel
+    {
+        [Required(ErrorMessage = "Это обязательное поле.")]
+        [EmailAddress(ErrorMessage = "Email адрес введен некорректно.")]
+        [DisplayName("Email адрес")]
+        public override string Email { get; set; }
+    }
+}
