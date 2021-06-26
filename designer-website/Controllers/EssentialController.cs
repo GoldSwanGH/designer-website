c8ba3947801bcc1e3291b34764b1908eb9f16394
amaterasuzxc <igarjar@gmail.com>
2021-06-26 15:02:31 +0300

    Added Essential Controller

---
+++ designer-website/Controllers/EssentialController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace designer_website.Controllers
+{
+    public class EssentialController : Controller
+    {
+        public IActionResult Authors()
+        {
+            return View();
+        }
+
+        public IActionResult Author()
+        {
+            return View();
+        }
+    }
+}
