d4e8d1078da20872b0a09a70bb2ef805e2cf1bc1
Lebedev Vladimir <goldswanru@gmail.com>
2021-06-26 18:50:13 +0300

    Authors action implemented

--- designer-website/Controllers/EssentialController.cs
+++ designer-website/Controllers/EssentialController.cs
@@ -1,12 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using designer_website.Models;
+using designer_website.Models.EntityFrameworkModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace designer_website.Controllers
 {
     public class EssentialController : Controller
     {
+        private readonly MSDBcontext _dbcontext;
+        public EssentialController(MSDBcontext dbcontext)
+        {
+           _dbcontext = dbcontext;
+        }
         public IActionResult Authors()
         {
-            return View();
+            var authors = _dbcontext.Users.Where(u =>
+                u.Role == _dbcontext.Roles.FirstOrDefault(r => r.RoleName == "Designer")).ToList();
+            
+            var model = new UserCollectionViewModel();
+            foreach (var user in authors)
+            {
+                model.Users.Add(new UserViewModel
+                {
+                    Email = user.Email,
+                    FirstName = user.FirstName,
+                    LastName = user.LastName,
+                    Tel = user.Tel
+                });
+            }
+            
+            return View(model);
         }
 
         public IActionResult Author()
