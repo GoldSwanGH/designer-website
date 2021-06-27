a3ee5f879cc3c3270b74ddfd01adfef5215ae357
Lebedev Vladimir <goldswanru@gmail.com>
2021-06-27 21:21:19 +0300

    OrderCollectionViewModel added

---
+++ designer-website/Models/ViewModels/OrderCollectionViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using designer_website.Models.EntityFrameworkModels;
+
+namespace designer_website.Models.ViewModels
+{
+    public class OrderCollectionViewModel
+    {
+        public List<OrderInfo> OrderInfoList { get; set; }
+    }
+}
