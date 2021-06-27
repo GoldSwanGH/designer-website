83009f435ab8ddded78eab636c052b6ab469ffc0
Lebedev Vladimir <goldswanru@gmail.com>
2021-06-27 22:17:08 +0300

    Fixes

--- designer-website/Models/ViewModels/NewOrderInitialModel.cs
+++ designer-website/Models/ViewModels/NewOrderInitialModel.cs
@@ -4,7 +4,7 @@ namespace designer_website.Models.ViewModels
 {
     public class NewOrderInitialModel
     {
-        public int ServiceId { get; set; }
+        public int? ServiceId { get; set; }
         
         public List<string> Designers { get; set; }
     }
