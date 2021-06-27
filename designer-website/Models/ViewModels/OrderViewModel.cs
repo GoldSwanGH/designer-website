0035b5f8f77de704e8abf2c829ac787dc06e2594
Lebedev Vladimir <goldswanru@gmail.com>
2021-06-27 20:12:28 +0300

    OrderViewModel implementation

--- designer-website/Models/ViewModels/OrderViewModel.cs
+++ designer-website/Models/ViewModels/OrderViewModel.cs
@@ -1,7 +1,16 @@
-﻿namespace designer_website.Models
+﻿using System;
+using System.Collections.Generic;
+using designer_website.Models.EntityFrameworkModels;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace designer_website.Models
 {
     public class OrderViewModel
     {
+        public List<Service> Services { get; set; }
         
+        public List<UserViewModel> Designers { get; set; }
+        
+        public string Description { get; set; }
     }
 }
