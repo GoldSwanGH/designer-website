a2234cf024e317466024a1bef0b4c963cb74faf5
Lebedev Vladimir <goldswanru@gmail.com>
2021-06-27 18:40:49 +0300

    ServiceCollectionViewModel added

---
+++ designer-website/Models/ViewModels/ServiceCollectionViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using designer_website.Models.EntityFrameworkModels;
+
+namespace designer_website.Models.ViewModels
+{
+    public class ServiceCollectionViewModel
+    {
+        public List<Service> Services { get; set; }
+    }
+}
