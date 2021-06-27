4f5d49b4e6866a0c06f086117bcaabf91d1e9b71
Lebedev Vladimir <goldswanru@gmail.com>
2021-06-27 23:07:19 +0300

    ViewModels refactoring

---
+++ designer-website/Models/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using designer_website.Models.EntityFrameworkModels;
+
+namespace designer_website.Models.ViewModels
+{
+    public class ProfileViewModel
+    {
+        public UserViewModel User { get; set; }
+        
+        public List<OrderDesignersViewModel> OrderInfos { get; set; }
+        
+        public List<UserWorksViewModel> Works { get; set; }
+    }
+}
---
+++ designer-website/Models/ViewModels/UserWorksViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using designer_website.Models.EntityFrameworkModels;
+
+namespace designer_website.Models.ViewModels
+{
+    public class UserWorksViewModel
+    {
+        public Work Work { get; set; }
+        public List<UserViewModel> Designers { get; set; }
+    }
+}
