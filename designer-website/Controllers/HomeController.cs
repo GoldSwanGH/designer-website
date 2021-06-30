e787aa7cd587e117970fd88ef5ad2c5b654d2a46
Lebedev Vladimir <goldswanru@gmail.com>
2021-06-30 18:50:25 +0300

    Final fixes

--- designer-website/Controllers/HomeController.cs
+++ designer-website/Controllers/HomeController.cs
@@ -31,7 +31,7 @@ namespace designer_website.Controllers
         {
             return View();
         }
-
+    
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
